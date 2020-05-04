c796254df6b692c8fb5e395d7676044dc2640d7a
Seraphim Bogomolov <seraphim.bogomolov@gmail.com>
2020-05-04 14:19:31 +0700

    install & setup postgres

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # ssh_secret
+
+```
+docker-compose build
+
+docker-compose up -d
+
+docker-compose logs -f
+
+docker-compose up -d --build
+
+docker-compose exec web python manage.py migrate --noinput
+
+```
--- app/ssh_secret/settings.py
+++ app/ssh_secret/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'ssh_secret.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': os.environ.get('SQL_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.environ.get('SQL_DATABASE', os.path.join(BASE_DIR, 'db.sqlite3')),
+        'USER': os.environ.get('SQL_USER', 'admin'),
+        'PASSWORD': os.environ.get('SQL_PASSWORD', 'P@ssw0rd'),
+        'HOST': os.environ.get('SQL_HOST', 'db'),
+        'PORT': os.environ.get('SQL_PORT', '5432'),
     }
 }
 
